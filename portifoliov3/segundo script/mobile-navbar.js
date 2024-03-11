32cd7c55ecb45ecb359abb8208ee1d3ed6c2f0af
Rayan <141056772+Rayan757575@users.noreply.github.com>
2024-03-11 15:54:17 -0300

    Add files via upload

---
+++ portifoliov3/segundo script/mobile-navbar.js
@@ -0,0 +1,76 @@
+class MobileNavbar {
+    constructor(mobileMenu, navlist, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navlist);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            var tempo = (index / 6 + 0.3);
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = 'navLinkFade 0.5s ease forwards ' + tempo +'s');
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.mobileMenu.addEventListener("click", this.handleClick);
+        }
+        return this;
+    }
+}
+class DarkMode {
+    constructor(icon) {
+        this.icon = document.querySelector(icon);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    ChangeToDark(){
+        document.body.style.setProperty('--background-main', '#121212');
+        document.body.style.setProperty('--background-menu', '#02070c');
+        document.body.style.setProperty('--text-color', 'rgb(253, 249, 249)');
+        document.body.style.setProperty('--span-color', '#007FFF');
+        document.body.style.setProperty('--border-menu-color', 'white');
+    }
+    ChangeToLight(){
+        document.body.style.setProperty('--background-main', ' white');
+        document.body.style.setProperty('--background-menu', ' rgb(253, 253, 253)'); 
+        document.body.style.setProperty('--text-color', ' black');
+        document.body.style.setProperty('--span-color', 'rgb(10, 99, 244)');
+        document.body.style.setProperty('--border-menu-color', 'rgb(123, 123, 123)');
+    }
+
+    handleClick() {
+       if(this.icon.src == "https://rayancp.vercel.app/imagens/sun.png"){
+            this.icon.src = "imagens/moon.png";
+            this.ChangeToDark();
+        }else{
+            this.icon.src = "imagens/sun.png";
+            this.ChangeToLight();
+        }
+    }
+
+    init() {
+        if (this.icon) {
+            this.icon.addEventListener("click", this.handleClick);
+        }
+        return this;
+    }
+}
+const mobileNavbar = new MobileNavbar(".mobile-menu", ".nav-list", ".nav-list li")
+const darkmode = new DarkMode(".icon")
+mobileNavbar.init();
+darkmode.init();
