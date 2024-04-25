eecc60062e79d7347690d9fc3669a35e6835b1f9
Rayan <141056772+Rayan757575@users.noreply.github.com>
2024-04-25 10:44:25 -0300

    Update mobile-navbar.js
    changing scheme colors

--- portifoliov3/segundo script/mobile-navbar.js
+++ portifoliov3/segundo script/mobile-navbar.js
@@ -39,10 +39,10 @@ class DarkMode {
     }
 
     ChangeToDark(){
-        document.body.style.setProperty('--background-main', '#161616');
+        document.body.style.setProperty('--background-main', '#171717');
         document.body.style.setProperty('--background-menu', '#121212');
         document.body.style.setProperty('--text-color', 'rgb(253, 249, 249)');
-        document.body.style.setProperty('--span-color', '#007FFF');
+        document.body.style.setProperty('--span-color', '#4493f8');
         document.body.style.setProperty('--link-source-color', '#4493f8');
     }
     ChangeToLight(){
